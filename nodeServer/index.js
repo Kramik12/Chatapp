005b407cbb4ea782cef136b3ef97d754fdbe98d7
Kramik <kramikkafle7@gmail.com>
2024-05-05 20:21:35 +0545

    first commit

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,1 +1,15 @@
 //Node server which will handle Socket io connection
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
+    });
+})
